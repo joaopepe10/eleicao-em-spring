d5614d56d5af38319983c9e293c2e5e3f2a5c6a0
João Perdro Pires <joaopedropires61@gmail.com>
2024-03-26 18:15:36 -0300

    Inicializando repositorio

---
+++ src/test/java/joaopepe10/com/github/eleicao/EleicaoApplicationTests.java
@@ -0,0 +1,13 @@
+package joaopepe10.com.github.eleicao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EleicaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
