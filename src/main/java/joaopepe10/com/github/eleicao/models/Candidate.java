23c8dd1c36b7b7fbcf149aff4e102daf0ffd7a9b
João Perdro Pires <joaopedropires61@gmail.com>
2024-03-26 23:12:19 -0300

    feat: Create entity's BaseEntity, Candidate and User

---
+++ src/main/java/joaopepe10/com/github/eleicao/models/Candidate.java
@@ -0,0 +1,10 @@
+package joaopepe10.com.github.eleicao.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class Candidate extends BaseEntity{
+    private String speech;
+}
---
+++ src/main/java/joaopepe10/com/github/eleicao/models/User.java
@@ -0,0 +1,10 @@
+package joaopepe10.com.github.eleicao.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class User extends BaseEntity{
+    public String name;
+}
