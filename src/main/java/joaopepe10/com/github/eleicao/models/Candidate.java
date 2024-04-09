609d7894f8b0b2b38888e03adb6ef42a361012f8
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-08 23:05:43 -0300

    feat: Create relationship and column speech not null

--- src/main/java/joaopepe10/com/github/eleicao/models/Candidate.java
+++ src/main/java/joaopepe10/com/github/eleicao/models/Candidate.java
@@ -1,10 +1,18 @@
 package joaopepe10.com.github.eleicao.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.ManyToOne;
 import lombok.Getter;
 import lombok.Setter;
 
 @Getter
 @Setter
+@Entity
 public class Candidate extends BaseEntity{
+    @Column(nullable = false)
     private String speech;
+
+    @ManyToOne
+    private User user;
 }
