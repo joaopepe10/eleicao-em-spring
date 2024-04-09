07ffed3560cce134743682ff2a3831d89a937720
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-08 23:03:00 -0300

    feat: Created DTO for User entity

---
+++ src/main/java/joaopepe10/com/github/eleicao/dto/CreateUserDto.java
@@ -0,0 +1,10 @@
+package joaopepe10.com.github.eleicao.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class CreateUserDto {
+    private String name;
+}
---
+++ src/main/java/joaopepe10/com/github/eleicao/dto/ReadUserDto.java
@@ -0,0 +1,11 @@
+package joaopepe10.com.github.eleicao.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReadUserDto {
+    private long id;
+    private String name;
+}
