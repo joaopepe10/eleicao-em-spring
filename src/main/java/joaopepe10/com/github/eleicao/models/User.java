95a5df0bd11e3eae048074f67458b9b067b6c91e
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-08 23:04:15 -0300

    feat: Define name for table and column name not null

--- src/main/java/joaopepe10/com/github/eleicao/models/User.java
+++ src/main/java/joaopepe10/com/github/eleicao/models/User.java
@@ -1,10 +1,23 @@
 package joaopepe10.com.github.eleicao.models;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.List;
+
 @Getter
 @Setter
+@Entity
+@Table(name = "users")
 public class User extends BaseEntity{
-    public String name;
+    @Column(nullable = false)
+    private String name;
+
+    @OneToMany(mappedBy = "user")
+    private List<Candidate> candidates;
+
 }
