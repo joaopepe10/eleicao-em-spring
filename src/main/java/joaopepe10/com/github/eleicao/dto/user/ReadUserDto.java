1892ca255fe363e8f2d94eb0b8951d22ca09a89f
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-10 19:51:05 -0300

    feat: Update in ReadUserDto

---
+++ src/main/java/joaopepe10/com/github/eleicao/dto/user/ReadUserDto.java
@@ -0,0 +1,11 @@
+package joaopepe10.com.github.eleicao.dto.user;
+
+import joaopepe10.com.github.eleicao.dto.BaseDto;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ReadUserDto extends BaseDto {
+    private String name;
+}
