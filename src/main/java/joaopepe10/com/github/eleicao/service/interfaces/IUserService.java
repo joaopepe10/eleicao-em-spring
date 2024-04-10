85eecf2c7f5565dca07af3a9b17fc5f9395a2e5f
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-10 19:56:06 -0300

    feat: Insert findById

--- src/main/java/joaopepe10/com/github/eleicao/service/interfaces/IUserService.java
+++ src/main/java/joaopepe10/com/github/eleicao/service/interfaces/IUserService.java
@@ -1,12 +1,13 @@
 package joaopepe10.com.github.eleicao.service.interfaces;
 
-import joaopepe10.com.github.eleicao.dto.CreateUserDto;
-import joaopepe10.com.github.eleicao.dto.ReadUserDto;
+import joaopepe10.com.github.eleicao.dto.user.CreateUserDto;
+import joaopepe10.com.github.eleicao.dto.user.ReadUserDto;
 
 import java.util.List;
 
 public interface IUserService {
-    ReadUserDto findById(long id);
     ReadUserDto save(CreateUserDto dto);
+    ReadUserDto findById(Long id);
     List<ReadUserDto> findAll();
+
 }
