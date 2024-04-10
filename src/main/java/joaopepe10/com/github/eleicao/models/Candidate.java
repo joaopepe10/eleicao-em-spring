9bc157f682bffbd01baf42dfce76523587a72b61
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-10 19:06:04 -0300

    feat: Create relationship with Election

--- src/main/java/joaopepe10/com/github/eleicao/models/Candidate.java
+++ src/main/java/joaopepe10/com/github/eleicao/models/Candidate.java
@@ -3,16 +3,24 @@ package joaopepe10.com.github.eleicao.models;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.ManyToOne;
+import lombok.AllArgsConstructor;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 
 @Getter
 @Setter
 @Entity
+@NoArgsConstructor
+@AllArgsConstructor
 public class Candidate extends BaseEntity{
     @Column(nullable = false)
     private String speech;
 
     @ManyToOne
     private User user;
+
+    @ManyToOne
+    private Election election;
+
 }
