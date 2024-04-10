474c4e071cf5d0c77a7e6f8ca6a94780815521e5
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-10 19:57:44 -0300

    feat: Insert @JsonIgnore

--- src/main/java/joaopepe10/com/github/eleicao/models/User.java
+++ src/main/java/joaopepe10/com/github/eleicao/models/User.java
@@ -1,5 +1,6 @@
 package joaopepe10.com.github.eleicao.models;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.OneToMany;
@@ -18,6 +19,7 @@ public class User extends BaseEntity{
     private String name;
 
     @OneToMany(mappedBy = "user")
+    @JsonIgnore
     private List<Candidate> candidates;
 
 }
