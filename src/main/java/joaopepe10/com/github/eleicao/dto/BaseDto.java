55cc3a3e1724da69d0ff6385611b0362430e7269
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-10 18:59:18 -0300

    refactor: Update in BaseDto and BaseEntity

--- src/main/java/joaopepe10/com/github/eleicao/dto/BaseDto.java
+++ src/main/java/joaopepe10/com/github/eleicao/dto/BaseDto.java
@@ -1,5 +1,13 @@
 package joaopepe10.com.github.eleicao.dto;
 
+import lombok.Getter;
+import lombok.Setter;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
 public abstract class BaseDto {
-    public Long id;
+    protected Long id;
+    protected LocalDateTime createdAt;
 }
--- src/main/java/joaopepe10/com/github/eleicao/models/BaseEntity.java
+++ src/main/java/joaopepe10/com/github/eleicao/models/BaseEntity.java
@@ -5,12 +5,19 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.MappedSuperclass;
 import lombok.Getter;
+import org.hibernate.annotations.CreationTimestamp;
+import org.springframework.data.annotation.CreatedDate;
+
+import java.time.LocalDateTime;
 
 @Getter
 @MappedSuperclass
 public abstract class BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    public Long id;
+    protected Long id;
+
+    @CreationTimestamp
+    protected LocalDateTime createdAt;
 
 }
