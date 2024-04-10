b7ec51a284978c743e41ffd73298331110b0d0a4
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-10 19:55:39 -0300

    refactor: Update imports

--- src/main/java/joaopepe10/com/github/eleicao/controller/UserController.java
+++ src/main/java/joaopepe10/com/github/eleicao/controller/UserController.java
@@ -1,7 +1,7 @@
 package joaopepe10.com.github.eleicao.controller;
 
-import joaopepe10.com.github.eleicao.dto.CreateUserDto;
-import joaopepe10.com.github.eleicao.dto.ReadUserDto;
+import joaopepe10.com.github.eleicao.dto.user.CreateUserDto;
+import joaopepe10.com.github.eleicao.dto.user.ReadUserDto;
 import joaopepe10.com.github.eleicao.service.UserServiceImpl;
 import joaopepe10.com.github.eleicao.service.interfaces.IUserService;
 import org.springframework.http.ResponseEntity;
