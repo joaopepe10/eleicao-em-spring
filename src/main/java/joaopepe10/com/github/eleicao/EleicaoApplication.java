0bc7542369dd1d5b59f328f535a86871226b4b0d
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-14 20:27:38 -0300

    feat: Able JpaAuditing

--- src/main/java/joaopepe10/com/github/eleicao/EleicaoApplication.java
+++ src/main/java/joaopepe10/com/github/eleicao/EleicaoApplication.java
@@ -2,7 +2,9 @@ package joaopepe10.com.github.eleicao;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
+@EnableJpaAuditing
 @SpringBootApplication
 public class EleicaoApplication {
 
