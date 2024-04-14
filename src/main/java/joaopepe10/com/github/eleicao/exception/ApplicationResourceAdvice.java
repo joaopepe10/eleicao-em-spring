427d0d3aeffeaaa30d661b364a366370e26bc521
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-14 20:09:07 -0300

    feat: Update return of exception with statusCode and path of resource

--- src/main/java/joaopepe10/com/github/eleicao/exception/ApplicationResourceAdvice.java
+++ src/main/java/joaopepe10/com/github/eleicao/exception/ApplicationResourceAdvice.java
@@ -1,5 +1,6 @@
 package joaopepe10.com.github.eleicao.exception;
 
+import jakarta.servlet.http.HttpServletRequest;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -9,13 +10,29 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class ApplicationResourceAdvice {
     @ExceptionHandler(BadRequestException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleBadRequestException(BadRequestException e){
-        return new ApiError(e.getMessage());
+    public ApiError handleBadRequestException(BadRequestException e, HttpServletRequest request){
+        String uri = request.getRequestURI();
+        return new ApiError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), uri);
     }
 
     @ExceptionHandler(IdNotFoundException.class)
     @ResponseStatus(HttpStatus.BAD_REQUEST)
-    public ApiError handleIdNotFoundException(IdNotFoundException e){
-        return new ApiError(e.getMessage());
+    public ApiError handleIdNotFoundException(IdNotFoundException e, HttpServletRequest request){
+        String uri = request.getRequestURI();
+        return new ApiError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), uri);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleIllegalArgumentException(IllegalArgumentException e, HttpServletRequest request){
+        String uri = request.getRequestURI();
+        return new ApiError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), uri);
+    }
+
+    @ExceptionHandler(RuntimeException.class)
+    @ResponseStatus(HttpStatus.BAD_REQUEST)
+    public ApiError handleIllegalArgumentException(RuntimeException e, HttpServletRequest request){
+        String uri = request.getRequestURI();
+        return new ApiError(HttpStatus.BAD_REQUEST.value(), e.getMessage(), uri);
     }
 }
