a1a46e0247e9414d18c471150c5df58928bdda13
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-14 20:29:57 -0300

    refactor: Update name method in IUserService

--- src/main/java/joaopepe10/com/github/eleicao/service/interfaces/IUserService.java
+++ src/main/java/joaopepe10/com/github/eleicao/service/interfaces/IUserService.java
@@ -3,11 +3,7 @@ package joaopepe10.com.github.eleicao.service.interfaces;
 import joaopepe10.com.github.eleicao.dto.user.CreateUserDto;
 import joaopepe10.com.github.eleicao.dto.user.ReadUserDto;
 
-import java.util.List;
-
 public interface IUserService {
     ReadUserDto save(CreateUserDto dto);
-    ReadUserDto findById(Long id);
-    List<ReadUserDto> findAll();
-
+    ReadUserDto findByIdAndReturnReadDto(Long id);
 }
