15920c621fcee5a02402206f9eb788db8d021c92
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-14 20:09:36 -0300

    feat: Create dto for update speech

---
+++ src/main/java/joaopepe10/com/github/eleicao/dto/candidate/UpdateSpeechCandidateDto.java
@@ -0,0 +1,10 @@
+package joaopepe10.com.github.eleicao.dto.candidate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class UpdateSpeechCandidateDto {
+    private String speech;
+}
