70db901839bce38861858e9c78ecc8b2bfc2a22c
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-14 20:32:57 -0300

    feat: Defines contract for ICandidateService

--- src/main/java/joaopepe10/com/github/eleicao/service/interfaces/ICandidateService.java
+++ src/main/java/joaopepe10/com/github/eleicao/service/interfaces/ICandidateService.java
@@ -2,10 +2,14 @@ package joaopepe10.com.github.eleicao.service.interfaces;
 
 import joaopepe10.com.github.eleicao.dto.candidate.CreateCandidateDto;
 import joaopepe10.com.github.eleicao.dto.candidate.ReadCandidateDto;
+import joaopepe10.com.github.eleicao.dto.candidate.UpdateSpeechCandidateDto;
+import joaopepe10.com.github.eleicao.models.Candidate;
 
 import java.util.List;
 
 public interface ICandidateService {
     ReadCandidateDto save(CreateCandidateDto dto);
-    List<ReadCandidateDto> findAll();
+    List<Candidate> findAll();
+
+    ReadCandidateDto updateSpeech(Long id, UpdateSpeechCandidateDto dto);
 }
