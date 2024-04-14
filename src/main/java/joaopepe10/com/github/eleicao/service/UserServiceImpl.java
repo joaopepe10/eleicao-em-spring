6ef55614f71329520c939b1ccda7b2aecfe9caa9
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-14 20:37:50 -0300

    refactor: Alter name method for findByIdAndReturnReadDto

--- src/main/java/joaopepe10/com/github/eleicao/service/UserServiceImpl.java
+++ src/main/java/joaopepe10/com/github/eleicao/service/UserServiceImpl.java
@@ -1,7 +1,7 @@
 package joaopepe10.com.github.eleicao.service;
 
-import joaopepe10.com.github.eleicao.dto.CreateUserDto;
-import joaopepe10.com.github.eleicao.dto.ReadUserDto;
+import joaopepe10.com.github.eleicao.dto.user.CreateUserDto;
+import joaopepe10.com.github.eleicao.dto.user.ReadUserDto;
 import joaopepe10.com.github.eleicao.models.User;
 import joaopepe10.com.github.eleicao.repository.UserRepository;
 import joaopepe10.com.github.eleicao.service.base.ServiceBaseImp;
@@ -9,31 +9,23 @@ import joaopepe10.com.github.eleicao.service.interfaces.IUserService;
 import org.modelmapper.ModelMapper;
 import org.springframework.stereotype.Service;
 
-import java.util.List;
-
 @Service
 public class UserServiceImpl extends ServiceBaseImp<User> implements IUserService {
 
     private final ModelMapper modelMapper;
-    public UserServiceImpl(UserRepository repository, ModelMapper modelMapper) {
-        super(repository);
+    private final UserRepository userRepository;
+    public UserServiceImpl(ModelMapper modelMapper, UserRepository userRepository) {
+        super(userRepository);
         this.modelMapper = modelMapper;
+        this.userRepository = userRepository;
     }
 
-    @Override
-    public ReadUserDto findById(long id) {
-        return null;
-    }
-
-    @Override
     public ReadUserDto save(CreateUserDto dto) {
         User entity = modelMapper.map(dto, User.class);
         save(entity);
         return modelMapper.map(entity, ReadUserDto.class);
     }
-
-    @Override
-    public List<ReadUserDto> findAll() {
-        return findAll();
+    public ReadUserDto findByIdAndReturnReadDto(Long id){
+        return modelMapper.map(findById(id), ReadUserDto.class);
     }
 }
