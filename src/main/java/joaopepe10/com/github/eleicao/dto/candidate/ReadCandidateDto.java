277d7befee672ff965b9c11b9aad5ba46e2889c4
João Perdro Pires <joaopedropires61@gmail.com>
2024-04-14 22:50:02 -0300

    refactor: Add fields for return of ReadCandidateDto

--- src/main/java/joaopepe10/com/github/eleicao/dto/candidate/ReadCandidateDto.java
+++ src/main/java/joaopepe10/com/github/eleicao/dto/candidate/ReadCandidateDto.java
@@ -1,20 +1,18 @@
 package joaopepe10.com.github.eleicao.dto.candidate;
 
 import joaopepe10.com.github.eleicao.dto.BaseDto;
-import joaopepe10.com.github.eleicao.models.User;
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-
-import java.time.LocalDateTime;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor
 public class ReadCandidateDto extends BaseDto {
+    private Long userId;
+    private String candidateName;
     private String speech;
-    private User user;
-
+    private Long electionId;
 }
